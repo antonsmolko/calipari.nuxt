8951133436a6be22fcd7b9432d936cad8ed5b13c
Anton Smolko <asmolko@yandex.ru>
2020-08-04 23:59:13 +0300

    fixed: set border radius from gravatar on bottom-bar

--- components/layout/BottomBar.vue
+++ components/layout/BottomBar.vue
@@ -1,28 +1,28 @@
 <template lang="pug">
-    client-only
-        .tm-navbar.tm-bottombar.uk-position-bottom.uk-position-fixed.uk-background-secondary.uk-light(
-            class="uk-hidden@l")
-            ul.tm-iconnav.uk-navbar-nav.uk-flex.uk-flex-middle.uk-flex-between
-                li: nuxt-link(to="/" data-uk-icon="home")
-                    span.tm-bottombar__item-title Главная
-                li: nuxt-link(to="/catalog" data-uk-icon="album")
-                    span.tm-bottombar__item-title Каталог
-                li: nuxt-link(to="/search" data-uk-icon="search")
-                    span.tm-bottombar__item-title Поиск
-                li: nuxt-link(to="/cart" data-uk-icon="bag")
-                    span.uk-badge(v-if="cartQty") {{ cartQty }}
-                    span.tm-bottombar__item-title Корзина
-                li: nuxt-link(to="/wishlist" data-uk-icon="heart")
-                    span.uk-badge(v-if="likeQty") {{ likeQty }}
-                    span.tm-bottombar__item-title Wishlist
-                template(v-if="$auth.loggedIn")
-                    li: nuxt-link.tm-logged-in(to="/profile")
-                        span.tm-bottombar__item-title Профиль
-                        .tm-navbar__gravatar
-                            img(:data-src="$auth.user.gravatar_small" :alt="$auth.user.name" data-uk-img)
-                template(v-else)
-                    li: nuxt-link(to="/login" data-uk-icon="user")
-                        span.tm-bottombar__item-title Войти
+  client-only
+    .tm-navbar.tm-bottombar.uk-position-bottom.uk-position-fixed.uk-background-secondary.uk-light(
+      class="uk-hidden@l")
+      ul.tm-iconnav.uk-navbar-nav.uk-flex.uk-flex-middle.uk-flex-between
+        li: nuxt-link(to="/" data-uk-icon="home")
+          span.tm-bottombar__item-title Главная
+        li: nuxt-link(to="/catalog" data-uk-icon="album")
+          span.tm-bottombar__item-title Каталог
+        li: nuxt-link(to="/search" data-uk-icon="search")
+          span.tm-bottombar__item-title Поиск
+        li: nuxt-link(to="/cart" data-uk-icon="bag")
+          span.uk-badge(v-if="cartQty") {{ cartQty }}
+          span.tm-bottombar__item-title Корзина
+        li: nuxt-link(to="/wishlist" data-uk-icon="heart")
+          span.uk-badge(v-if="likeQty") {{ likeQty }}
+          span.tm-bottombar__item-title Wishlist
+        template(v-if="$auth.loggedIn")
+          li: nuxt-link.tm-logged-in(to="/profile")
+            span.tm-bottombar__item-title Профиль
+            .tm-navbar__gravatar
+              img(:data-src="$auth.user.gravatar_small" :alt="$auth.user.name" data-uk-img)
+        template(v-else)
+          li: nuxt-link(to="/login" data-uk-icon="user")
+            span.tm-bottombar__item-title Войти
 </template>
 <script>
 export default {
@@ -39,84 +39,94 @@ export default {
 
 <style lang="scss">
 .uk-navbar-nav {
-    &.tm-iconnav {
-        .tm-bottombar & {
-            > li {
-                min-width: inherit;
-                flex-grow: 1;
-                > a, > span {
-                    .tm-bottombar__item-title {
-                        padding-top: 5px;
-                        line-height: 1;
-                    }
-                    min-height: $bottom-bar-height !important;
-                    font-size: 9px;
-                    padding: 0 $global-small-margin;
-                    color: $global-inverse-color;
-                    .uk-badge {
-                        min-width: 14px;
-                        height: 14px;
-                        top: 9px;
-                        left: 55%;
-                        font-size: 10px;
-                        font-weight: 600;
-                        color: $global-color;
-                        padding: 0 3px;
-                        background-color: $global-inverse-color;
-                    }
+  &.tm-iconnav {
+    .tm-bottombar & {
+      > li {
+        min-width: inherit;
+        flex-grow: 1;
 
-                    @include media-mob($s) {
-                        font-size: 10px;
-                        .uk-badge {
-                            left: 53%;
-                        }
-                    }
-                    @include media-mob($s) {
-                        font-size: 12px;
-                        .tm-bottombar__item-title {
-                            padding-top: 3px;
-                        }
-                    }
-                    @include media-mob($m) {
-                        .uk-badge {
-                            left: 52%;
-                        }
-                    }
-                }
+        > a, > span {
+          .tm-bottombar__item-title {
+            padding-top: 5px;
+            line-height: 1;
+          }
 
-                &:first-child {
-                    border-left: none;
-                }
+          min-height: $bottom-bar-height !important;
+          font-size: 9px;
+          padding: 0 $global-small-margin;
+          color: $global-inverse-color;
 
-                &:last-child {
-                    border-right: none;
-                }
+          .uk-badge {
+            min-width: 14px;
+            height: 14px;
+            top: 9px;
+            left: 55%;
+            font-size: 10px;
+            font-weight: 600;
+            color: $global-color;
+            padding: 0 3px;
+            background-color: $global-inverse-color;
+          }
+
+          @include media-mob($s) {
+            font-size: 10px;
+            .uk-badge {
+              left: 53%;
             }
+          }
+          @include media-mob($s) {
+            font-size: 12px;
+            .tm-bottombar__item-title {
+              padding-top: 3px;
+            }
+          }
+          @include media-mob($m) {
+            .uk-badge {
+              left: 52%;
+            }
+          }
         }
-    }
-    &:hover {
-        &::before {
-            display: none;
+
+        &:first-child {
+          border-left: none;
         }
+
+        &:last-child {
+          border-right: none;
+        }
+      }
     }
+  }
+
+  &:hover {
+    &::before {
+      display: none;
+    }
+  }
 }
 
 .tm-bottombar {
-    border-bottom: none;
-    z-index: 2000;
-    .uk-navbar, .uk-navbar-item {
-        min-height: $bottom-bar-height;
-    }
+  border-bottom: none;
+  z-index: 2000;
+
+  .uk-navbar, .uk-navbar-item {
+    min-height: $bottom-bar-height;
+  }
 }
+
 .tm-logged-in {
-    justify-content: flex-end;
-    .tm-navbar__gravatar {
-        width: 20px;
-        height: 20px;
-    }
-    .tm-bottombar__item-title {
-        padding-top: 3px;
-    }
+  justify-content: flex-end;
+
+  .tm-navbar__gravatar {
+    width: 20px;
+    height: 20px;
+    border-radius: 500px;
+    overflow: hidden;
+  }
+
+  .tm-bottombar__item-title {
+    padding-top: 3px;
+  }
 }
 
 </style>
