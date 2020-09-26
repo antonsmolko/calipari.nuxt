ffdfcc36c14a011aaa6f31ed5a72681b0a68e60a
Anton Smolko <asmolko@yandex.ru>
2020-09-26 10:46:39 +0300

    update: nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,9 +29,9 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      ...(isDev && {
+      ...[isDev && {
         name: 'robots', content: 'noindex, nofollow'
-      })
+      }]
     ],
     link: [
       { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' }
