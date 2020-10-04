814bfa1696677b769c85a9f9c16cd18a560adfda
Anton Smolko <asmolko@yandex.ru>
2020-10-04 12:59:54 +0300

    add: favicons

--- components/mixins/pageMeta.js
+++ components/mixins/pageMeta.js
@@ -22,10 +22,28 @@ export default {
           vmid: 'keywords',
           name: 'keywords',
           content: this.page.keywords || ''
-        }
+        },
+        { name: 'msapplication-TileColor', content: '#292C30' },
+        { name: 'msapplication-TileColor', content: '/favicon/ms-icon-144x144.png' },
+        { name: 'theme-color', content: '#292C30' }
       ],
       link: [
-        { rel: 'canonical', href: process.env.BASE_URL + this.fullPath }
+        { rel: 'canonical', href: process.env.BASE_URL + this.fullPath },
+        { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '57x57', href: '/favicon/apple-icon-57x57.png' },
+        { rel: 'apple-touch-icon', sizes: '60x60', href: '/favicon/apple-icon-60x60.png' },
+        { rel: 'apple-touch-icon', sizes: '72x72', href: '/favicon/apple-icon-72x72.png' },
+        { rel: 'apple-touch-icon', sizes: '76x76', href: '/favicon/apple-icon-76x76.png' },
+        { rel: 'apple-touch-icon', sizes: '114x114', href: '/favicon/apple-icon-114x114.png' },
+        { rel: 'apple-touch-icon', sizes: '120x120', href: '/favicon/apple-icon-120x120.png' },
+        { rel: 'apple-touch-icon', sizes: '144x144', href: '/favicon/apple-icon-144x144.png' },
+        { rel: 'apple-touch-icon', sizes: '152x152', href: '/favicon/apple-icon-152x152.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-icon-180x180.png' },
+        { rel: 'apple-touch-icon', sizes: '192x192', href: '/favicon/apple-icon-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon/favicon-96x96.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+        { rel: 'manifest', href: '/favicon/manifest.json' }
       ]
     }
   },
--- nuxt.config.js
+++ nuxt.config.js
@@ -32,10 +32,27 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'theme-color', content: '#093256' },
-      ...[isDev && { name: 'robots', content: 'noindex, nofollow' }]
+      ...[isDev && { name: 'robots', content: 'noindex, nofollow' }],
+      { name: 'msapplication-TileColor', content: '#292C30' },
+      { name: 'msapplication-TileColor', content: '/favicon/ms-icon-144x144.png' },
+      { name: 'theme-color', content: '#292C30' }
     ],
     link: [
-      { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '57x57', href: '/favicon/apple-icon-57x57.png' },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: '/favicon/apple-icon-60x60.png' },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: '/favicon/apple-icon-72x72.png' },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: '/favicon/apple-icon-76x76.png' },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/favicon/apple-icon-114x114.png' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/favicon/apple-icon-120x120.png' },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/favicon/apple-icon-144x144.png' },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/favicon/apple-icon-152x152.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-icon-180x180.png' },
+      { rel: 'apple-touch-icon', sizes: '192x192', href: '/favicon/apple-icon-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon/favicon-96x96.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+      { rel: 'manifest', href: '/favicon/manifest.json' }
     ]
   },
   rootDir: __dirname,
@@ -110,6 +127,21 @@ export default {
     ['@nuxtjs/dotenv', { systemvars: true }],
     '@nuxtjs/pwa'
   ],
+  /*
+  ** PWA
+   */
+  // pwa: {
+  //   meta: {
+  //     title: 'Calipari',
+  //     author: 'Anton Smolko',
+  //     theme_color: '#292C30'
+  //   },
+  //   manifest: {
+  //     name: 'Calipari application',
+  //     short_name: 'Calipari app',
+  //     lang: 'ru'
+  //   }
+  // },
   /*
   ** Nuxt.js modules
   */
